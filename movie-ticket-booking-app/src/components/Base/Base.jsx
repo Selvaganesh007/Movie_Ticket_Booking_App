e2bbe0fc9b4c3b3eec8a8ea5bf869d37933e160a
Selvaganesh M <selvakvpvmk@gmail.com>
2025-01-04 17:36:06 +0530

    base

--- movie-ticket-booking-app/src/components/Base/Base.jsx
+++ movie-ticket-booking-app/src/components/Base/Base.jsx
@@ -1,10 +1,7 @@
 import './Base.scss';
-import { useState } from 'react';
 import LandingPage from '../LandingPage/LandingPage.jsx';
 
 function Base() {
-    const [count, setCount] = useState(0)
-  
     return (
       <>
         <LandingPage />
